d7ceb7d3495efdbdb523003e10cfc9b79100c6c4
matthieu-geley <deb@debian>
2022-10-14 14:27:58 +0200

    modification Job 9

--- Job 9/accessrights_1.sh
+++ Job 9/accessrights_1.sh
@@ -2,13 +2,13 @@ filename="Userlist.csv"
 
 echo -e "étape 1 fait\n"
 
-while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6
+while IFS=","" " read rec_column1 rec_column2 rec_column3 rec_column4 rec_column5
 
 do
 
 echo -e "étape 2 fait\n"
 
-if [ "$rec_column5" == "Admin" ]
+if [[ "$rec_column5" =~ .*Admin.* ]]
 
 then
 
@@ -26,6 +26,6 @@ G="Plateformeurs"
 
 fi
 
-sudo useradd $rec_column2 --uid $rec_column1 --create-home --groups $G --shell /bin/bash && sudo echo $rec_column5 | passwd $rec_column2
+sudo useradd -p $rec_column4 $rec_column2 --uid $rec_column1 --create-home --groups $G --shell /bin/bash
 
 done < <(tail -n +2 $filename)
